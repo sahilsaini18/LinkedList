d44038d8cf16ae6693c95db4353e61699aaaa60c
sahilsaini18 <157702038+sahilsaini18@users.noreply.github.com>
2024-02-06 14:47:43 +0530

    LinkedListEx1.java

--- src/linkedList/LinkedListEx1.java
+++ src/linkedList/LinkedListEx1.java
@@ -1,41 +1,45 @@
 package linkedList;
-import java.util.*;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+
 /**
- * Operations performing with linked list
- * linked list uses doubly linked list internally
- * linked list can act as a list and queue as it implements both the list
- * and the deque interface
+ * Operations performing with linked list Linked list uses doubly linked list
+ * internally Linked list can act as a list and queue as it implements both the
+ * list and the deque interface
  */
 public class LinkedListEx1 {
-public static void main(String args[]) {
-	LinkedList<String> al = new LinkedList<String>();
-	//adding elements to linked list
-	al.add("Ravi");
-	al.add("vijay");
-	al.add("Ravi");
-	al.add("Ajay");
-	LinkedList<String> al2= new LinkedList<String>();
-	al2.add("sahil");
-	al2.add("saini");
-	al2.add("Ajay");
-	//merging two list
-	al.addAll(al2);
-	
-	//removing first element
-	al.removeFirst();
-	//removing last element
-	al.removeLast();
-	al.removeFirstOccurrence("Ajay");
-	Iterator<String> itr=al.iterator();
-	while(itr.hasNext()) {
-		System.out.println(itr.next());
+	private static final Logger log = Logger.getLogger(LinkedListEx1.class.getName());
+
+	public static void main(String args[]) {
+		LinkedList<String> al = new LinkedList<String>();
+		// Adding elements to linked list
+		al.add("Ravi");
+		al.add("Vijay");
+		al.add("Ravi");
+		al.add("Ajay");
+		LinkedList<String> al2 = new LinkedList<String>();
+		al2.add("Sahil");
+		al2.add("Saini");
+		al2.add("Ajay");
+		// Merging two lists
+		al.addAll(al2);
+
+		// Removing first element
+		al.removeFirst();
+		// Removing last element
+		al.removeLast();
+		al.removeFirstOccurrence("Ajay");
+		Iterator<String> itr = al.iterator();
+		while (itr.hasNext()) {
+			log.info(itr.next());
+		}
+		log.info("After reversing");
+		// To traverse the list of elements in reverse order
+		Iterator<String> it = al.descendingIterator();
+		while (it.hasNext()) {
+			log.info(it.next());
+		}
 	}
-	System.out.println("after reversing");
-	// to traverse the list of elements in reverse order
-	Iterator<String> it = al.descendingIterator();
-	while(it.hasNext()) {
-		System.out.println(it.next());
-	}
-	
-}
 }
